a8414bab2fcbc22e7a6fdae33d945d37bc418e28
Richard Yang <rich.yang88@gmail.com>
2019-10-19 22:48:52 -0400

    getting server and route error

---
+++ client/build/precache-manifest.f6d515934a604e2c22f6528f7fde110a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "716f26499f4f3252d715e06b16fe6e69",
+    "url": "/index.html"
+  },
+  {
+    "revision": "10c9f56bcba07049e6e6",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "f8d2d9344fe2689b427f",
+    "url": "/static/js/2.2f9dfdf9.chunk.js"
+  },
+  {
+    "revision": "10c9f56bcba07049e6e6",
+    "url": "/static/js/main.f9855888.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
